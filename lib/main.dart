b66c9f6496b4d8ac233388bdefb1a4873b07c2cf
FlutLab.io <>
2020-12-04 09:10:58 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/taffyapp/generatedpopupwidget/GeneratedPopupWidget.dart';
+import 'package:flutterapp/taffyapp/generatededit_profilewidget/GeneratedEdit_ProfileWidget.dart';
+import 'package:flutterapp/taffyapp/generatedemojionev1candywidget/GeneratedEmojionev1candyWidget.dart';
+import 'package:flutterapp/taffyapp/generatedmy_profile2widget/GeneratedMy_Profile2Widget.dart';
+import 'package:flutterapp/taffyapp/generatedprofilescreenwidget/GeneratedProfileScreenWidget.dart';
+import 'package:flutterapp/taffyapp/generatedchatwidget/GeneratedChatWidget.dart';
+import 'package:flutterapp/taffyapp/generatedchatgoldwidget/GeneratedChatGoldWidget.dart';
+import 'package:flutterapp/taffyapp/generatedtaffygoldhomewidget/GeneratedTaffyGoldHOmeWidget.dart';
+import 'package:flutterapp/taffyapp/generated1weekwidget/Generated1WeekWidget.dart';
+import 'package:flutterapp/taffyapp/generatedtaffygoldwidget/GeneratedTaffyGoldWidget.dart';
+import 'package:flutterapp/taffyapp/generatedmessageswidget5/GeneratedMessagesWidget5.dart';
+import 'package:flutterapp/taffyapp/generateddetailmessageswidget/GeneratedDetailMessagesWidget.dart';
+import 'package:flutterapp/taffyapp/generatedview_profile2widget/GeneratedView_Profile2Widget.dart';
+import 'package:flutterapp/taffyapp/generatedview_profilewidget/GeneratedView_ProfileWidget.dart';
+import 'package:flutterapp/taffyapp/generateddiscovergoldwidget/GeneratedDiscoverGoldWidget.dart';
+import 'package:flutterapp/taffyapp/generateddiscover19widget/GeneratedDiscover19Widget.dart';
+import 'package:flutterapp/taffyapp/generateddiscover18widget/GeneratedDiscover18Widget.dart';
+import 'package:flutterapp/taffyapp/generatedlikewidget21/GeneratedLikeWidget21.dart';
+import 'package:flutterapp/taffyapp/generatednope_likewidget/GeneratedNope_LikeWidget.dart';
+import 'package:flutterapp/taffyapp/generatedwidget31/GeneratedWidget31.dart';
+import 'package:flutterapp/taffyapp/generatedaddprofilewidget/GeneratedADDPROFILEWidget.dart';
+import 'package:flutterapp/taffyapp/generatedwidget36/GeneratedWidget36.dart';
+import 'package:flutterapp/taffyapp/generatedwidget38/GeneratedWidget38.dart';
+import 'package:flutterapp/taffyapp/generatedwidget40/GeneratedWidget40.dart';
+import 'package:flutterapp/taffyapp/generated2widget2/Generated2Widget2.dart';
+import 'package:flutterapp/taffyapp/generatedwidget49/GeneratedWidget49.dart';
+import 'package:flutterapp/taffyapp/generatedwidget54/GeneratedWidget54.dart';
+import 'package:flutterapp/taffyapp/generated2widget3/Generated2Widget3.dart';
+import 'package:flutterapp/taffyapp/generated2widget4/Generated2Widget4.dart';
+import 'package:flutterapp/taffyapp/generated1widget2/Generated1Widget2.dart';
+import 'package:flutterapp/taffyapp/generatedloginswidget/GeneratedLoginsWidget.dart';
+import 'package:flutterapp/taffyapp/generatedframewidget/GeneratedFrameWidget.dart';
+import 'package:flutterapp/taffyapp/generatedgoldwidget/GeneratedGoldWidget.dart';
+import 'package:flutterapp/taffyapp/generatedframe1widget/GeneratedFrame1Widget.dart';
+import 'package:flutterapp/taffyapp/generatedbrokenheartwidget/GeneratedBrokenHeartWidget.dart';
+import 'package:flutterapp/taffyapp/generatedmdi_addwidget/GeneratedMdi_addWidget.dart';
+import 'package:flutterapp/taffyapp/generatedimage3widget/GeneratedImage3Widget.dart';
+
+void main() {
+  runApp(TaffyApp());
+}
+
+class TaffyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedFrameWidget',
+      routes: {
+        '/GeneratedPopupWidget': (context) => GeneratedPopupWidget(),
+        '/GeneratedEdit_ProfileWidget': (context) =>
+            GeneratedEdit_ProfileWidget(),
+        '/GeneratedEmojionev1candyWidget': (context) =>
+            GeneratedEmojionev1candyWidget(),
+        '/GeneratedMy_Profile2Widget': (context) =>
+            GeneratedMy_Profile2Widget(),
+        '/GeneratedProfileScreenWidget': (context) =>
+            GeneratedProfileScreenWidget(),
+        '/GeneratedChatWidget': (context) => GeneratedChatWidget(),
+        '/GeneratedChatGoldWidget': (context) => GeneratedChatGoldWidget(),
+        '/GeneratedTaffyGoldHOmeWidget': (context) =>
+            GeneratedTaffyGoldHOmeWidget(),
+        '/Generated1WeekWidget': (context) => Generated1WeekWidget(),
+        '/GeneratedTaffyGoldWidget': (context) => GeneratedTaffyGoldWidget(),
+        '/GeneratedMessagesWidget5': (context) => GeneratedMessagesWidget5(),
+        '/GeneratedDetailMessagesWidget': (context) =>
+            GeneratedDetailMessagesWidget(),
+        '/GeneratedView_Profile2Widget': (context) =>
+            GeneratedView_Profile2Widget(),
+        '/GeneratedView_ProfileWidget': (context) =>
+            GeneratedView_ProfileWidget(),
+        '/GeneratedDiscoverGoldWidget': (context) =>
+            GeneratedDiscoverGoldWidget(),
+        '/GeneratedDiscover19Widget': (context) => GeneratedDiscover19Widget(),
+        '/GeneratedDiscover18Widget': (context) => GeneratedDiscover18Widget(),
+        '/GeneratedLikeWidget21': (context) => GeneratedLikeWidget21(),
+        '/GeneratedNope_LikeWidget': (context) => GeneratedNope_LikeWidget(),
+        '/GeneratedWidget31': (context) => GeneratedWidget31(),
+        '/GeneratedADDPROFILEWidget': (context) => GeneratedADDPROFILEWidget(),
+        '/GeneratedWidget36': (context) => GeneratedWidget36(),
+        '/GeneratedWidget38': (context) => GeneratedWidget38(),
+        '/GeneratedWidget40': (context) => GeneratedWidget40(),
+        '/Generated2Widget2': (context) => Generated2Widget2(),
+        '/GeneratedWidget49': (context) => GeneratedWidget49(),
+        '/GeneratedWidget54': (context) => GeneratedWidget54(),
+        '/Generated2Widget3': (context) => Generated2Widget3(),
+        '/Generated2Widget4': (context) => Generated2Widget4(),
+        '/Generated1Widget2': (context) => Generated1Widget2(),
+        '/GeneratedLoginsWidget': (context) => GeneratedLoginsWidget(),
+        '/GeneratedFrameWidget': (context) => GeneratedFrameWidget(),
+        '/GeneratedGoldWidget': (context) => GeneratedGoldWidget(),
+        '/GeneratedFrame1Widget': (context) => GeneratedFrame1Widget(),
+        '/GeneratedBrokenHeartWidget': (context) =>
+            GeneratedBrokenHeartWidget(),
+        '/GeneratedMdi_addWidget': (context) => GeneratedMdi_addWidget(),
+        '/GeneratedImage3Widget': (context) => GeneratedImage3Widget(),
+      },
+    );
+  }
+}
